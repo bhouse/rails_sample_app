f1e89e4ef77fbf2c80a4425fc1be58cf7867ecab
Ben House <github@quintessons.com>
2013-05-28 22:05:28 -0700

    fixing syntax errors related to test refactor

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,7 +8,7 @@ describe "StaticPages" do
 
   shared_examples_for "all static pages" do
     it { should have_selector('h1',     text: heading) }
-    it { should have_selector('title',  text: full_title(page_title) }
+    it { should have_selector('title',  text: full_title(page_title)) }
   end
 
   describe "Home page" do
@@ -17,7 +17,7 @@ describe "StaticPages" do
     let(:page_title)  { '' }
 
     it_should_behave_like "all static pages"
-    it { shoud_not have_selector 'title', text '| Home' }
+    it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "Help page" do
